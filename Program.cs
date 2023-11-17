a02b21c87a0e27d7adf098ffb6b9bebe6c9497dc
ReZvanYY <141616463+ReZvanYY@users.noreply.github.com>
2023-11-17 14:27:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            var randomNumber = rand.Next(0, 100);
+            int guessedNumber = 101;
+                Console.WriteLine("I am thinking of a number between 0 and 100. Guess it!");
+
+            while (guessedNumber != randomNumber)
+            {
+                guessedNumber = Convert.ToInt32(Console.ReadLine());
+                if (guessedNumber == randomNumber)
+                {
+                    Console.WriteLine("Correct! You guessed the number!");
+                }
+                else if (guessedNumber < randomNumber)
+                {
+                    Console.WriteLine("Too low, try again!");
+                }
+                else if (guessedNumber > randomNumber)
+                {
+                    Console.WriteLine("Too high, try again!");
+                }
+
+            }
+        }
+
+    }
+}
